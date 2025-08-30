dc4a50797c209346d54a46c542071449df762796
Ethan Colgan <ethancolgan@pm.me>
2025-08-30 17:46:11 +0930

    prepare for SSR (server side rendering) deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind({ applyBaseStyles: true }), mdx()],
+  output: 'server',  // server side rendering (SSR)]
+  adapter: node({ mode: 'standalone' }),  // bundle a self contained server
+  integrations: [tailwind({ applyBaseStyles: true }), mdx()],  
   srcDir: 'src',
   server: { host: true }
 });
