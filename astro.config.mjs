baeb5ed361378620fade1cd6349123386f0f3a06
Ethan Colgan <ethancolgan@pm.me>
2025-08-30 20:47:04 +0930

    Gave astro.config.mjs a mode dependent on whether there is a environment variable with a mode otherwise defaults to middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import node from '@astrojs/node';
 
+const mode = process.env.ASTRO_ADAPTER_MODE || 'middleware';
+
 export default defineConfig({
   output: 'server',
-  adapter: node(),
+  adapter: node({ mode }),
   // adapter: node({ mode: 'standalone' }),
   integrations: [tailwind({ applyBaseStyles: true }), mdx()],
   srcDir: 'src',
