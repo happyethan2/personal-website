2b98b06c5538996ea6c34f9b55f0c577ecbdaba8
Ethan Colgan <ethancolgan@pm.me>
2025-08-30 21:36:57 +0930

    content update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import node from '@astrojs/node';
 
-// const mode = process.env.ASTRO_ADAPTER_MODE || 'standalone';
-
 export default defineConfig({
   output: 'server',
-  // adapter: node({ mode }),
   adapter: node({ mode: 'standalone' }),
   integrations: [tailwind({ applyBaseStyles: true }), mdx()],
   srcDir: 'src',
